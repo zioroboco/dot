0d0d348ae46bf60e0c1237540bc390f19326e6e8
Louis Quinnell <_@ziorobo.co>
2021-03-13 16:57:12 +1100

    Prettier repl prompt

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -18,3 +18,17 @@ function subtypetree(roottype, level = 1, indent = 4)
   end
 end
 
+using OhMyREPL
+OhMyREPL.input_prompt!("❱ ", :yellow)
+OhMyREPL.enable_pass!("RainbowBrackets", false)
+colorscheme!("JuliaDefault")
+
+# Set the shell prompt (red, by default)
+Base.Threads.@spawn @sync begin
+  sleep(0.5)
+  Base.active_repl.interface.modes[2].prompt="❱ "
+  Base.active_repl.interface.modes[3].prompt="? "
+end
+
+# Leave a blank line
+println()
