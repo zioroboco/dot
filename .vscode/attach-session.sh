39187998c963d296d3e3211dbbc512cb011d052f
Louis Quinnell <_@ziorobo.co>
2021-05-30 19:48:37 +1000

    Fix tmux session creation for notes workspace

--- .vscode/attach-session.sh
+++ .vscode/attach-session.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+tmux="/usr/local/bin/tmux"
 session=$(basename "$(project-root "$1")")
 
-if [[ -z $session ]]; then
+if [[ $(basename $(pwd)) = "notes" ]]; then
+	session="notes"
+elif [[ -z $session ]]; then
   session="λ"
 fi
 
-tmux attach-session -t $session || tmux new-session -s $session
+$tmux attach-session -t $session || $tmux new-session -s $session
 
