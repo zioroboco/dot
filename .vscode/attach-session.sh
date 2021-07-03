89e2ce14ef475a649cefce811037a4cec5a6a86f
Louis Quinnell <_@ziorobo.co>
2021-07-03 12:12:41 +1000

    Make vscode tmux sessions start in the workspace directory

--- .vscode/attach-session.sh
+++ .vscode/attach-session.sh
@@ -2,13 +2,13 @@
 set -e
 
 tmux="/usr/local/bin/tmux"
-session=$(basename "$(project-root "$1")")
+project_dir=$(project-root "$1")
+session=$(basename "$project_dir")
 
-if [[ $(basename $(pwd)) = "notes" ]]; then
+if [[ $(basename "$(pwd)") = "notes" ]]; then
 	session="notes"
 elif [[ -z $session ]]; then
-  session="λ"
+	session="λ"
 fi
 
-$tmux attach-session -t $session || $tmux new-session -s $session
-
+$tmux attach-session -t $session || $tmux new-session -s $session -t "$project_dir"
