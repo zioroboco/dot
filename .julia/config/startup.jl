819faf84b02711767d9fa43c457cbfa52eb0dbe5
Louis Quinnell <_@ziorobo.co>
2021-07-03 12:12:16 +1000

    Use nvim as julia builtin editor

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,23 +1,3 @@
-# note: not working in julia 1.6 (yet)
-# https://github.com/julia-vscode/julia-vscode/wiki/Known-issues-and-workarounds#revisejl
-# atreplinit() do repl
-#     @async try
-#         sleep(0.1)
-#         @eval using Revise
-#         @async Revise.wait_steal_repl_backend()
-#     catch
-#         @warn("Could not load Revise.")
-#     end
-# end
-
-function subtypetree(roottype, level = 1, indent = 4)
-  level == 1 && println(roottype)
-  for s in subtypes(roottype)
-    println(join(fill(" ", level * indent)) * string(s))
-    subtypetree(s, level + 1, indent)
-  end
-end
-
 using OhMyREPL
 OhMyREPL.input_prompt!("❱ ", :yellow)
 OhMyREPL.enable_pass!("RainbowBrackets", false)
@@ -32,3 +12,6 @@ end
 
 # Leave a blank line
 println()
+
+ENV["JULIA_EDITOR"] = "nvim"
+
